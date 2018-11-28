1982e16ba791f772230d86fe1879efd6ce618419
UKS <skulptorrrr@gmail.com>
2018-11-28 04:55:39 +0200

    BetaApp commit. Code refactoring needed.

---
+++ TestProj/Guesser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Guessing
+{
+    public class Guesser
+    {
+        // Used to record play history
+        int[,,] _matrix;
+        int lw1, lw2;
+        int _sequentialLosesCounter;
+
+        Random _rnd;
+
+        /// <summary>
+        /// Expectation which number will be choosen by Human
+        /// </summary>
+        public int Prediction { get; private set; }
+
+        public int ScoreCpu = 0;
+        public int ScoreHuman = 0;
+        public int GamesCounter { get { return ScoreCpu + ScoreHuman; } }
+        public float ScoreCpuInPercents { get { return ScoreCpu / ((float)GamesCounter / 100); } }
+
+        private int _playerChoice;
+
+        public Guesser()
+        {
+            _rnd = new Random(DateTime.Now.Millisecond);
+
+            _matrix = new int[2, 2, 2] { { { 0, 0 }, { 0, 0 } }, { { 0, 0 }, { 0, 0 } } };
+
+            // Random value as we have no data to get Prediction
+            Prediction = SimulateCoinFlip();
+
+            // Won or lost last play and play before last
+            lw2 = lw1 = 0;
+        }
+
+        public bool IsWinnerIsHuman(int playerChoice)
+        {
+            _playerChoice = playerChoice;
+
+            return IsWinnerIsHuman();
+        }
+
+        private bool IsWinnerIsHuman()
+        {
+            return (_playerChoice != Prediction);
+        }
+
+        public void CalcNextPrediction()
+        {
+            if (IsWinnerIsHuman())
+            {
+                ScoreHuman++;
+                _sequentialLosesCounter++;
+            }
+            else
+            {
+                ScoreCpu++;
+                _sequentialLosesCounter = 0;
+            }
+
+            WriteLatestScoresToMatrix();
+            CalculatePrediction();
+        }
+
+        private void WriteLatestScoresToMatrix()
+        {
+            _matrix[lw2, lw1, 1] = (_matrix[lw2, lw1, 0] == _playerChoice ? 1 : 0);
+            _matrix[lw2, lw1, 0] = _playerChoice;
+
+            lw2 = lw1;
+            lw1 = _playerChoice;
+        }
+
+        /// <summary>
+        /// If lost more than twice in present state, chose random strategy;  Otherwise, keep same strategy
+        /// </summary>
+        private void CalculatePrediction()
+        {
+            Prediction = (_matrix[lw2, lw1, 1] == 1 && _sequentialLosesCounter < 2)
+                         ? _matrix[lw2, lw1, 0]
+                         : SimulateCoinFlip();
+        }
+
+        private int SimulateCoinFlip()
+        {
+            return _rnd.Next(0, 2);
+        }
+    }
+}
---
+++ TestProj/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guessing
+{
+    class ShannonGuessing
+    {        
+        static void Main(string[] args)
+        {
+            char chr;
+            Guesser g = new Guesser();
+
+            //читаем данные - то число которое загадал человек
+            do
+            {
+                Console.WriteLine("Write only 0 or 1;\t\tPress 5 to exit");
+
+                chr = Console.ReadKey().KeyChar;
+                Console.WriteLine("");
+
+                if (chr == '5')
+                {
+                    Environment.Exit(0);
+                }
+                else if (chr == '1' || chr == '0')
+                {
+                    var humanNumChoose = (int)Char.GetNumericValue(chr);
+                    
+                    if (g.IsWinnerIsHuman(humanNumChoose))
+                    {
+                        Console.Write("You WIN! \t");
+                    }
+                    else
+                    {
+                        Console.Write("LOOSER!; \t");
+                    }
+
+                    Console.WriteLine($"Human:{g.ScoreHuman} : CPU:{g.ScoreCpu};\t % of CPU wins:{g.ScoreCpuInPercents}");
+
+                    g.CalcNextPrediction();
+                }
+            } while (true);
+        }
+    }
+
+
+}
+
+
